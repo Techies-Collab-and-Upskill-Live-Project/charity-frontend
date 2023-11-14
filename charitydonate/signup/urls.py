44dad29193b66f649e7e7c8ec18d811085985cab
josie <josie9447@gmail.com>
2023-11-14 17:44:37 +0100

    added the signup functions

--- charitydonate/signup/urls.py
+++ charitydonate/signup/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.homepage, name='home'),
+    path('register', views.Registration, name='register'),
+    path('login', views.LoginView, name='login'),
+    path('logout', views.LogoutView, name='logout')
 ]
