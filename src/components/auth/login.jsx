b4fced35ae48611fc9a6b04986912df45aa535a1
Lomoee <favoursunday600@gmail.com>
2023-12-28 10:31:57 +0100

    login

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -10,36 +10,45 @@ const Login = () =>{
         <div>
             <Navber />
 
-            <div className="flex flex-col">
-                <h3 className="mt-28" >Welcome Back</h3>
+            <div className="flex flex-col pl-96 mb-28 max-sm:pl-0">
+                <h3 className="mt-28 ml-40 mb-10 font-medium text-3xl max-sm:text-teal-500 max-sm:ml-24 "  >Welcome Back</h3>
 
-               <form action="" className="">
-                    <button><img src={googleicon} alt="" />  Sign in with Google</button>
-                    <button><img src={facebookicon} alt="" />  Sign in with Facebook</button>
+               <form action="" className="pr-80 mr-7 max-sm:pr-0 max-sm:mr-6 max-sm:ml-6">
+                   <div className="flex flex-col">
+                    <button className="bg-white rounded-md text-sm mb-5 border-2 text-center h-11 font-medium" style={{width: "100%"}}><img src={googleicon} alt="" style={{display: "inline"}} width={20} className="mr-1"/>  Sign in with Google</button>
+                    <button className="bg-blue-500 border rounded-md text-sm h-11 font-medium text-white" style={{width: "100%"}}><img src={facebookicon} alt="" style={{display: "inline"}} width={20} className="mr-1"/>  Sign in with Facebook</button>
+                   </div>
+
+                   <div className="flex">
+                      <hr class="h-1 w-56  bg-gray-100 border-0 rounded mt-10 mb-6 dark:bg-gray-700 ml-5"/>
+                      <p className="ml-8 mt-7">OR</p>
+                    <hr class="w-56 h-1 bg-gray-100 border-0 rounded mt-10 mb-6 dark:bg-gray-700 ml-8" />
+                   </div>
 
                     <div >
                       <div>
                         <label htmlFor="email" className="mb-10">Email</label>
-                        <input type="email" name="email" id="" placeholder="olivia@untitledui.com" className="block  rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2 mb-5 w-96" />
+                        <input type="email" name="email" id="" placeholder="olivia@untitledui.com" className="block  rounded-md border-0 py-1.5 pl-3 pr-20 text-black ring-1 ring-inset ring-gray-300 placeholder:text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2 mb-5 " style={{width: "100%", height: "50px"}} />
                       </div>
 
                       <div>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="passsword" id="" placeholder="************" className="block  rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2 mb-5 w-96" />
+                        <input type="password" name="passsword" id="" placeholder="***********************" className="block  rounded-md border-0 py-1.5 pl-3 pr-20 text-black ring-1 ring-inset ring-gray-300 placeholder:text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2 mb-5 w-96" style={{width: "100%", height: "50px"}}/>
                       </div>
 
-                      <div>
-                        <div>
-                            <label htmlFor="checkbox">Stay Logged in</label>
-                            <input type="checkbox" name="checkbox" id="" />
+                      <div className="flex mt-0 justify-between">
+                        <div className="inline-flex">
+
+                        <input  id="disabled-checked-checkbox" type="checkbox" value="" className=""/>
+                            <label htmlFor="checkbox" className="font-medium ml-1">Stay Logged in</label>
                         </div>
 
-                        <div>
-                            <a href="/">Forget password?</a>
+                        <div className="">
+                            <a href="/" className="text-teal-500">Forget password?</a>
                         </div>
                       </div>
 
-                      <button type="submit" className="bg-teal-500 rounded-md px-3 text-sm text-white mt-5 w-96 h-10"> Submit</button>
+                      <button type="submit" className="bg-teal-500 rounded-md px-3 text-sm text-white mt-5 w-96 h-10" style={{width: "100%"}}> Login</button>
 
                     </div>
                </form>
