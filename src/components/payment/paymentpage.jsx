3d7ef72229a6cff9b21a42f35f3165f78be4c06b
Lomoee <favoursunday600@gmail.com>
2024-01-06 01:32:01 +0100

    payment page

--- src/components/payment/paymentpage.jsx
+++ src/components/payment/paymentpage.jsx
@@ -1,8 +1,11 @@
 import lefticon from "../../assets/icons/arrow-left.png";
 import React, { useState } from 'react';
 import Select from 'react-select';
+import {explorecategorylist} from "../../Data";
+import numdonatedimg from "../../assets/icons/numdonated.png"
 
 const PaymentPage = () => {
+    const selectedActivity = explorecategorylist[1];
   const [selectedAmount, setSelectedAmount] = useState(0);
   const [selectedCurrency, setSelectedCurrency] = useState('USD'); // Default currency
 
@@ -17,7 +20,8 @@ const PaymentPage = () => {
   ];
 
   return (
-    <div>
+    <div className="flex">
+      <div>
       <div>
         <img src={lefticon} alt="" />
         <p>Back to Fundraiser</p>
@@ -54,11 +58,76 @@ const PaymentPage = () => {
             <option value="USD">USD</option>
             <option value="EUR">EUR</option>
            </select>
+           <p>This is a hint text to help user.</p>
           </div>
+
+          <div>
+            <h3>Personal Information</h3>
+            <p>Enter your name; it will appear as anonymous if left blank</p>
+
+            <label for="name">Name</label>
+            <input type="text" name="name"/>
+
+            <label htmlFor="paymetmethod">Payment</label>
+            <p>Choose your preferable payment option, in order to proceed with your donation</p>
+            <select name="paymetmethod" id="">
+                <option value="--select">--select</option>
+                <option value="credit card">Credit card / Debit card / Bank transfer</option>
+                <option value="Cryptocurrency">Cryptocurrency</option>
+            </select>
+
+            <input type="checkbox" name="checkbox"/>
+            <label htmlFor="checkbox">I have read, understood and I agree to Donation Trace’s
+             <a href="/">terms of service</a>
+             and
+             <a href="/">privacy</a>
+            statement</label>
+
+            <div>
+                <button>Process to payment gateway</button>
+                <button>Donate with crypto</button>
+            </div>
+
+            <h3>Fruad policy</h3>
+            <p>Lorem ipsum dolor sit amet consectetur. Diam mattis sit risus ultrices vivamus. Leo velit ut arcu et scelerisque netus varius. Cursus a facilisi turpis urna elementum velit fringilla risus scelerisque. Fringilla justo egestas sociis sem diam ante platea. Faucibus </p>
+          </div>
+
         </div>
+      </div>
+      </div>
+
+      <div>
+        <h3>Donation Title/Name</h3>
+        <img src={selectedActivity.CoverImg} alt="" />
+
+
+        <div className="flex mt-3">
+                    <img src={numdonatedimg} alt="" />
+                    <p className="mt-1 ml-3 font-bold text-orange-400">5k people just donated</p>
+        </div>
+
+        <h3>Donation Summary</h3>
+        <div>
+            <p>Your donation</p>
+            <p>$1000.00</p>
+        </div>
+
+        <div>
+            <h3>Service fee (2%)</h3>
+            <p>$20.00</p>
+        </div>
+
+        <hr />
+
+        <div>
+            <h5>Total</h5>
+            <h5>$1020.00</h5>
+        </div>
+
       </div>
     </div>
   );
 }
 
 export default PaymentPage;
+
