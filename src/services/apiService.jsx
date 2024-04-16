cb73c8432b3977fb95e2b8a38a0cf9ffa2cd9e82
Samuel Ogboye <ogboyesam@gmail.com>
2024-04-16 08:13:36 +0100

    update

--- src/services/apiService.jsx
+++ src/services/apiService.jsx
@@ -4,7 +4,14 @@ import dayjs from "dayjs";
 import { useContext } from "react";
 import AuthContext from "../auth/context/AuthContext";
 
+function trimURL(url) {
+  return url.replace(/\/v1(?=\/|$)/, "");
+}
+
 const baseURL = process.env.REACT_APP_API_URL;
+const refreshURL = trimURL(baseURL);
+console.log("baseURL", baseURL);
+console.log("refreshURL", refreshURL);
 
 const useApiService = () => {
   const { authTokens, setUser, setAuthTokens } = useContext(AuthContext);
@@ -17,12 +24,18 @@ const useApiService = () => {
   axiosInstance.interceptors.request.use(async (req) => {
     const user = jwtDecode(authTokens.access);
     const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+    console.log("isExpired", isExpired);
 
-    if (!isExpired) return req;
-
-    const response = await axios.post(`${baseURL}/token/refresh/`, {
+    //     if (!isExpired) return req;
+    console.log("access", authTokens.access);
+    const response = await axios.post(`${refreshURL}/token/refresh/`, {
       refresh: authTokens.refresh,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      Authorization: `Bearer ${authTokens.access}`,
     });
+    console.log("response", response);
     localStorage.setItem("authTokens", JSON.stringify(response.data));
 
     setAuthTokens(response.data);
